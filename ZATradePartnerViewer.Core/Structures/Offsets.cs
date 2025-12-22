e4025e6eb41587f7bb9fba071b8878c95b75bab2
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2025-12-22 20:04:10 +1100

    ZA 2.0.0

--- ZATradePartnerViewer.Core/Structures/Offsets.cs
+++ ZATradePartnerViewer.Core/Structures/Offsets.cs
@@ -3,12 +3,12 @@ namespace ZATradePartnerViewer.Core.Structures;
 public abstract class Offsets
 {
     public const string LegendsZAID                                    = "0100F43008C44000";
-    public const string LegendsZAGameVersion                           = "1.0.3";
+    public const string LegendsZAGameVersion                           = "2.0.0";
 
-    public static IReadOnlyList<long> PlayerMyStatusPointer   { get; } = [0x5F0E250, 0xA0, 0x40];
+    public static IReadOnlyList<long> PlayerMyStatusPointer   { get; } = [0x6105710, 0xA0, 0x40];
 
-    public static IReadOnlyList<long> TradePartnerDataPointer { get; } = [0x3F00058, 0x1D8, 0x30, 0xA0, 0x0];
-    public static IReadOnlyList<long> TradePartnerNIDPointer  { get; } = [0x5F112B0, 0x108];
+    public static IReadOnlyList<long> TradePartnerDataPointer { get; } = [0x40F73D8, 0x1D8, 0x30, 0xA0, 0x0];
+    public static IReadOnlyList<long> TradePartnerNIDPointer  { get; } = [0x6108630, 0x108];
 
     public const uint TradePartnerNIDShift                             = 0x30;
     public const uint TradePartnerTIDShift                             = 0x74;
